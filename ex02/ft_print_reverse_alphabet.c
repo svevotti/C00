13cebb3899270c1571cb5cc24a04a7f5e80d2cf8
Sveva Mazzari <sve@MBP-di-Sveva.local>
2022-10-15 11:06:37 +0200

    C00

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+  char i ;
+  i = 'a';
+  while ( i <= 'z')
+  {
+    ft_putchar(i);
+    i = i + 1;
+  }
+}
