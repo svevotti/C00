45675e640073db883ef1f37275ad17110aee7486
Sveva Mazzari <sve@MBP-di-Sveva.local>
2022-10-17 10:36:32 +0200

    push ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -3,30 +3,37 @@
 void ft_putchar(char c)
 {
 	write(1, &c, 1);
-} 
+}
+
+void print_number(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (!(a == '7' && b == '8' && c == '9'))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
 
 void ft_print_comb(void)
 {
-	char a;
-	char b;
-	char c;
+	char	a;
+	char	b;
+	char	c;
+
 	a = '0';
 	b = '1';
 	c = '2';
+
 	while (a < '8')
 	{
 		while ( b < '9')
 		{
 			while (c <= '9')
 			{
-				ft_putchar(a);
-				ft_putchar(b);
-				ft_putchar(c);
-				if (!(a == '7' && b == '8' && c == '9'))
-				{	
-					ft_putchar(',');
-					ft_putchar(' ');
-				}
+				print_number(a, b, c);
 				c = c + 1;
 			}
 			b = b + 1;
--- rush00/2rush00.c
+++ rush00/2rush00.c
@@ -2,7 +2,8 @@
 
 void ft_putchar(char c);
 
-void ft_print_square(int x, int y)
+
+void ft_print_rows(int x, int y)
 {
 	int x_len;
 	int y_wid;
@@ -13,36 +14,36 @@ void ft_print_square(int x, int y)
 	{
 		while (x_len <= x)
 		{
-			if (x_len == 1)
+			if (y_wid == 1)
 			{
-				ft_putchar('A');
-			}
-			else if (x_len == x)
-			{
-				ft_putchar('C');
-			}
-			else if	(x_len <= x - 2)
-			{	
-				if (y_wid == 1 || y_wid == y)
-				{			
-					ft_putchar('B');
+				if (x_len == 1)
+				{
+					ft_putchar('A');
+				}
+				else if (x_len == x)
+				{
+					ft_putchar('C');
 				}
 				else
-				{	
-					ft_putchar(' ');
+				{
+					ft_putchar('B');
 				}
+
 			}
-			x_len = x_len + 1;	
-		}	
-			
+
+			x_len = x_len + 1;
+		}
+
 		ft_putchar('\n');
 		y_wid = y_wid + 1;
 		x_len = 1;
 	}
 }
 
+
+
 int main(void)
 {
-	ft_print_square(6, 6);
+	ft_print_rows(1, 5);
 	ft_putchar('\n');
 }
